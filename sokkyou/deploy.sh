617e193955c26afdc81742355be31eecb61b7ee9
shibayu36 <shibayu36@gmail.com>
2018-09-16 11:27:43 +0900

    アプリケーションデプロイ

--- sokkyou/deploy.sh
+++ sokkyou/deploy.sh
@@ -4,9 +4,9 @@ source ./sokkyou/sokkyou.sh
 for REMOTE in ${BACKEND[@]}; do
   SLACK "deploy ($REMOTE $USER)"
 
-  rsync -avz --exclude-from=.gitignore --exclude='.git' -e 'ssh' . isucon@$REMOTE:/home/isucon/isubata/
+  rsync -avz --exclude-from=.gitignore --exclude='.git' -e 'ssh' . isucon@$REMOTE:/home/isucon/torb/
 
-  ssh isucon@$REMOTE "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata/webapp/perl && ~/local/perl/bin/carton install && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.perl.service && sudo sysctl -p"
+  ssh isucon@$REMOTE "cd /home/isucon/torb/webapp/perl && ~/local/perl/bin/carton install && sudo sudo systemctl restart torb.perl.service && sudo sysctl -p"
 
   SLACK ":ok_hand:"
 done
